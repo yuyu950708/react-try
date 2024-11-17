8e1cb098e0b9a53b8284cf29e56c016e4673a437
yuyu950708 <jieyu950708@gmail.com>
2024-11-17 16:41:04 +0800

    move file in root

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,35 +1,35 @@
-import style from "../styles/Layout.module.css"
+import style from "../styles/Layout.module.css";
 
-interface CardType{
-    imgUrl:string
-    imgTitle:string
-    travelTitle:string
-    price:string
-    qi:string
-    // imgUrl1:string
-    // imgTitle1:string
-    // travelTitle1:string
-    // price1:string
-    // qi1:string
+interface CardType {
+  imgUrl: string;
+  imgTitle: string;
+  travelTitle: string;
+  price: string;
+  qi: string;
 }
 
-export default function Card({imgUrl,imgTitle,travelTitle,price,qi}:(CardType)){
-    return(
-            <>
-            <div className={style.imgg}>
-                <img src={imgUrl} alt="" />
-                <h2>{imgTitle}</h2>
-            </div>
-            <div className={style.travelTitle}>
-                <div>
-                    <h3>{travelTitle}</h3>
-                </div>
-                <div className={style.price}>
-                    <h3>{price}</h3>
-                    <h4>{qi}</h4>
-                </div>
-            </div>
-            
-            </>
-    )
+export default function Card({
+  imgUrl,
+  imgTitle,
+  travelTitle,
+  price,
+  qi,
+}: CardType) {
+  return (
+    <>
+      <div className={style.imgg}>
+        <img src={imgUrl} alt="#" />
+        <h2>{imgTitle}</h2>
+      </div>
+      <div className={style.travelTitle}>
+        <div>
+          <h3>{travelTitle}</h3>
+        </div>
+        <div className={style.price}>
+          <h3>{price}</h3>
+          <h4>{qi}</h4>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,41 @@
-import style from "../styles/Navbar.module.css"
+import style from "../styles/Navbar.module.css";
 
-export default function(){
-    return(
-        <>
-        <section className={style.nav}>
-            <div>
-                <h1>自由最夯</h1>
-            </div>
-            <div className={style.navDiv}>
-                <h3><a href="#" className={style.navTitle}> 精選推薦</a></h3>
-                <h3><a href="#" className={style.navTitle}>日本</a></h3>
-                <h3><a href="#" className={style.navTitle}>東南亞</a></h3>
-                <h3><a href="#" className={style.navTitle}>島嶼/歐洲</a></h3>
-                <h3><a href="#" className={style.navTitle}>高雄出發</a></h3>
-            </div>
-        </section>
-        
-        </>
-    )
+export default function Navbar() {
+  return (
+    <>
+      <section className={style.nav}>
+        <div>
+          <h1>自由最夯</h1>
+        </div>
+        <div className={style.navDiv}>
+          <h3>
+            <a href="#" className={style.navTitle}>
+              {" "}
+              精選推薦
+            </a>
+          </h3>
+          <h3>
+            <a href="#" className={style.navTitle}>
+              日本
+            </a>
+          </h3>
+          <h3>
+            <a href="#" className={style.navTitle}>
+              東南亞
+            </a>
+          </h3>
+          <h3>
+            <a href="#" className={style.navTitle}>
+              島嶼/歐洲
+            </a>
+          </h3>
+          <h3>
+            <a href="#" className={style.navTitle}>
+              高雄出發
+            </a>
+          </h3>
+        </div>
+      </section>
+    </>
+  );
 }
