792d92feb816a7b1503bf7022ceb1fbea37e76af
yuyu950708 <jieyu950708@gmail.com>
2024-11-17 16:17:12 +0800

    move file in root

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,17 +6,16 @@ interface CardType{
     travelTitle:string
     price:string
     qi:string
-    imgUrl1:string
-    imgTitle1:string
-    travelTitle1:string
-    price1:string
-    qi1:string
+    // imgUrl1:string
+    // imgTitle1:string
+    // travelTitle1:string
+    // price1:string
+    // qi1:string
 }
 
-export default function Card({imgUrl,imgTitle,travelTitle,price,qi,imgUrl1,imgTitle1,travelTitle1,price1,qi1}:(CardType)){
+export default function Card({imgUrl,imgTitle,travelTitle,price,qi}:(CardType)){
     return(
-        <main className={style.maincss}>
-        <section className={style.sectionn}>
+            <>
             <div className={style.imgg}>
                 <img src={imgUrl} alt="" />
                 <h2>{imgTitle}</h2>
@@ -30,22 +29,7 @@ export default function Card({imgUrl,imgTitle,travelTitle,price,qi,imgUrl1,imgTi
                     <h4>{qi}</h4>
                 </div>
             </div>
-        </section>
-        <section className={style.sectionn}>
-            <div className={style.imgg}>
-                <img src={imgUrl1} alt="" />
-                <h2>{imgTitle1}</h2>
-            </div>
-            <div className={style.travelTitle}>
-                <div>
-                    <h3>{travelTitle1}</h3>
-                </div>
-                <div className={style.price}>
-                    <h3>{price1}</h3>
-                    <h4>{qi1}</h4>
-                </div>
-            </div>
-        </section>
-    </main>
+            
+            </>
     )
 }
